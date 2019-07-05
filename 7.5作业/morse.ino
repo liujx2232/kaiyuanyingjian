30bd3607290ab8d8ebb9165698e960733b6facc6
liujx2232 <35186524+liujx2232@users.noreply.github.com>
2019-07-05 09:11:36 +0800

    Add files via upload

---
+++ 7.5作业/morse.ino
@@ -0,0 +1,99 @@
+//小写字母转换morse码 17050510046
+#ifndef _MORSE_H
+#define _MORSE_H
+#include "Arduino.h"
+#include "Morse.h"
+Morse::Morse(int pin)
+{
+  pinMode(pin,OUTPUT);
+  _pin=pin;
+}
+
+void Morse::dot()
+{
+  digitalWrite(_pin,HIGH);
+  delay(250);
+  digitalWrite(_pin,LOW);
+  delay(250);
+}
+
+void Morse::dash()
+{
+  digitalWrite(_pin,HIGH);
+  delay(1000);
+  digitalWrite(_pin,LOW);
+  delay(250);
+}
+int incomingByte = 0; //for incoming serial data
+String comdata = "";//声明字符串变量
+void setup(){
+ Serial.begin(9600);//opens serial port,sets data rate to 9600 bps
+}
+
+void loop()
+{
+    while (Serial.available() > 0)  
+    {
+        comdata += char(Serial.read());  //串口输入接收字符串
+        delay(2);
+    }
+    int i;
+    for(i=1;i<1000;i++){
+    int b = comdata[i];
+    switch(b){
+      case 97:printf("*-");break;
+      case 98:printf("-***");break;
+      case 99:printf("-*-*");break;
+      case 100:printf("-** ");break;
+      case 101:printf("* ");break;
+      case 102:printf("**-*");break;
+      case 103:printf("--*");break;
+      case 104:printf("**** ");break;
+      case 105:printf("** ");break;
+      case 106:printf("*--- ");break;
+      case 107:printf("-*-");break;
+      case 108:printf("*-**");break;
+      case 109:printf("--");break;
+      case 110:printf("-*");break;
+      case 111:printf("---");break;
+      case 112:printf("*--*");break;
+      case 113:printf("--*-");break;
+      case 114:printf("*-*");break;
+      case 115:printf("***");break;
+      case 116:printf("-");break;
+      case 117:printf("**-");break;
+      case 118:printf("***-");break;
+      case 119:printf("*--");break;
+      case 120:printf("-**-");break;
+      case 121:printf("-*--");break;
+      case 122:printf("--**");break;
+      case 32:printf(" ");break; //空格
+      case 13:printf("/");break; //回车
+      }
+    
+     if (comdata.length() > 0)
+      {
+          Serial.println(comdata);
+          if (comdata == '.')morse.dot();
+          else if (comdata == '-')morse.dash();
+          else if (comdata == ' ')morse.w_space();
+          if (comdata != ' ' && str[i] != '*')morse.c_space();
+          comdata = "";
+      }
+      
+    
+  }
+}
+
+
+
+class Morse
+{
+  public:
+    Morse(int pin);
+    void dot();
+    void dash();
+  private:
+    int _pin;
+};
+#endif /*_MORSE_H*/
